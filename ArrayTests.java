78c032cc459025087fe87e50f560d90e31a4c504
Dubshott <55414361+Dubshott@users.noreply.github.com>
2023-01-25 13:49:12 -0800

    push

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlaceMultipleNums() {
+    int[] input1 = { 2, 3, 4 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{4,3,2}, input1);
+  }
+  
+  @Test
+  public void testReversedTwo() {
+  int[] input1 = { 5,3,1 };
+  assertArrayEquals(new int[]{1,3,5}, ArrayExamples.reversed(input1));
+  }
 }
